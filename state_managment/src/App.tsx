e48af469c062e9821c67f9c7c6a58863068a7c7c
Awel Eshetu <aweloe@gmail.com>
2024-12-06 16:41:28 +0200

    Update tests
     - update test when updating components
    
    Refs None

--- state_managment/src/App.tsx
+++ state_managment/src/App.tsx
@@ -4,18 +4,17 @@ import {useGetPostsQuery} from './features/posts/postsApi';
 import Post from './components/Post';
 import Link from '@mui/material/Link';
 import List from '@mui/material/List';
-import { Typography } from '@mui/material';
-
+import { CircularProgress, Typography } from '@mui/material';
 
 const App= ()=> {
  const { data , isFetching } = useGetPostsQuery();
-
- //TODO: add loading spinner
- console.log(isFetching);
+  
 
   return (
     <>
+     
       <div>
+        {isFetching && <CircularProgress />}
         <h4>React state management showcase using RTK</h4>
         <Typography variant="body2">Api used : <Link href="https://dummyjson.com/docs/posts" target="_blank" rel="noreferrer">Dummy json posts</Link></Typography>
         <Typography variant="body2">Number of posts fetched: {data?.posts.length}</Typography>
---
+++ state_managment/src/__test__/post.test.tsx
@@ -0,0 +1,31 @@
+import { screen, render } from '@testing-library/react';
+import Post from '../components/Post';
+
+const mockPost = {
+    id: 1,
+    title: "His mother had always taught him",
+    body: "His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this motto.",
+    tags: ["history", "american", "crime"],
+    reactions: {
+        likes: 192,
+        dislikes: 25
+    },
+    views: 305,
+    userId: 121
+};
+
+describe('Post', () => {
+    beforeEach(() => {
+        render(<Post post={mockPost} />);
+    });
+    it('renders post details correctly', () => {
+        const title = screen.getByText("His mother had always taught him");
+        const body = screen.getByText("His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this motto.");
+        const likes = screen.getByText("192");
+        const dislikes = screen.getByText("25");
+        expect(title).toBeDefined();
+        expect(body).toBeDefined();
+        expect(likes).toBeDefined();
+        expect(dislikes).toBeDefined();
+    });
+});
